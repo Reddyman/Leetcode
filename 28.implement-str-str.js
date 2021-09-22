bbd84cfdc791cfde04c041fecef972e369497a6e
Reddyman <39847338+Reddyman@users.noreply.github.com>
2021-09-22 00:25:08 -0400

    Added solution to problem 28

---
+++ 28.implement-str-str.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  let needleLen = needle.length
+  if (needleLen === 0) return 0
+  for(let i = 0; i <= haystack.length-needleLen; i++) {
+    if (haystack.slice(i, i + needleLen) === needle) {
+      return i
+    }
+  }
+  return -1
+};
+// @lc code=end
+
